2675ed950f5ae4d01382c6f60eff997a28919cb6
Shanaia <Shanaia0213@gmail.com>
2024-03-27 23:37:42 +0530

    junit7 commit

---
+++ C/src/junit7_231.java
@@ -0,0 +1,34 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class junit7_231 {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		 q7_231 dog = new q7_231("Ludo", "Labrador");
+	        assertEquals("Ludo", dog.getName());
+	        assertEquals("Labrador", dog.getBreed());
+	    }
+
+}
