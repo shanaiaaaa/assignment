c8fb77f6913adbab8960a7870c9d216db40e0680
Shanaia <Shanaia0213@gmail.com>
2024-03-27 14:20:27 +0530

    junit1 commit

---
+++ C/src/junit1_231.java
@@ -0,0 +1,36 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class junit1_231 {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		assertEquals(10, Q1_add.add(5,5));
+		}
+		@Test
+		public void test2() {
+			assertEquals(-6,Q1_add.add(-10, 4));
+		}
+
+}
