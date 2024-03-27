b1da4d536f6f8f2ac3918c7705aca17dc762ff05
Shanaia <Shanaia0213@gmail.com>
2024-03-27 14:34:19 +0530

    q2 commit

--- C/src/q2_231.java
+++ C/src/q2_231.java
@@ -2,8 +2,14 @@
 public class q2_231 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		 int num = 5; 
+	 
+	        long fact = 1;
+	        
+	        for (int i = 1; i <= num; i++) {
+	            fact= fact * i;
+	        }
+	        
+	        System.out.println("Factorial of " + num + " is: " + fact);
 	}
-
 }
